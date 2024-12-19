e69420d40ff583d69dd2f0ee51b20d9d955abd1a
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2024-12-19 16:41:20 +0100

    optimized part 1 to use memoization

--- src/problems/day19.rs
+++ src/problems/day19.rs
@@ -6,12 +6,14 @@ pub fn part_one() -> usize {
 
     let (towels, designs) = input.split_once("\r\n\r\n").unwrap();
 
-    let towels: Vec<&str> = towels.split(", ").collect();
+    let towels: HashSet<&str> = towels.split(", ").collect();
     let designs: Vec<&str> = designs.lines().collect();
+    let mut memo: HashMap<&str, bool> = HashMap::new();
+    memo.insert("", true);
 
     let mut count = 0;
     for design in designs {
-        if can_make(design, &towels) {
+        if can_make(design, &towels, &mut memo) {
             count += 1;
         }
     }
@@ -19,17 +21,27 @@ pub fn part_one() -> usize {
     count
 }
 
-fn can_make(target: &str, towels: &[&str]) -> bool {
-    if target.is_empty() {
-        return true;
+fn can_make<'a>(
+    target: &'a str,
+    towels: &HashSet<&str>,
+    memo: &mut HashMap<&'a str, bool>,
+) -> bool {
+    if memo.contains_key(target) {
+        return memo[target];
     }
-    
-    for towel in towels {
-        if target.starts_with(towel) && can_make(target.strip_prefix(towel).unwrap(), towels) {
+
+    // check all prefixes of size 1 through 8
+    // since the biggest towel is of size 8
+    for i in 0..target.len().min(8) {
+        let (prefix, suffix) = target.split_at(i + 1);
+
+        if towels.contains(&prefix) && can_make(suffix, towels, memo) {
+            memo.insert(target, true);
             return true;
         }
     }
-    
+
+    memo.insert(target, false);
     false
 }
 
@@ -42,19 +54,23 @@ pub fn part_two() -> usize {
     let towels: HashSet<&str> = towels.split(", ").collect();
     let designs: Vec<&str> = designs.lines().collect();
     let mut memo: HashMap<&str, usize> = HashMap::new();
-    
+
     // only one way to make the empty string
-    memo.insert("", 1); 
+    memo.insert("", 1);
 
     let mut ways = 0;
     for design in designs {
-        ways += get_ways_to_make(design, &towels, &mut memo);   
+        ways += get_ways_to_make(design, &towels, &mut memo);
     }
 
     ways
 }
 
-fn get_ways_to_make<'a>(target: &'a str, towels: &HashSet<&str>, memo: &mut HashMap<&'a str, usize>) -> usize {
+fn get_ways_to_make<'a>(
+    target: &'a str,
+    towels: &HashSet<&str>,
+    memo: &mut HashMap<&'a str, usize>,
+) -> usize {
     if memo.contains_key(target) {
         return memo[target];
     }

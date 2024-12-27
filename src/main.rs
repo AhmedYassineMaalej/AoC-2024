d6c4e197d08c4f553d243b28de5600387f9beee7
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2024-12-27 12:03:13 +0100

    added a macro to run both parts of a given day and another macro to generate the table in the read.me

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,69 @@ use std::time::Instant;
 mod problems;
 
 #[allow(unused)]
-macro_rules! run {
-    ($x: ident, 1) => {
-        problems::$x::part_one()
+const PROBLEM_NAMES: [&str; 25] = [
+    "Historian Hysteria",
+    "Red-Nosed Reports",
+    "Mull It Over",
+    "Ceres Search",
+    "Print Queue",
+    "Guard Gallivant",
+    "Bridge Repair",
+    "Resonant Collinearity",
+    "Disk Fragmenter",
+    "Hoof It",
+    "Plutonian Pebbles",
+    "Garden Groups",
+    "Claw Contraption",
+    "Restroom Redoubt",
+    "Warehouse Woes",
+    "Reindeer Maze",
+    "Chronospatial Computer",
+    "RAM Run",
+    "Linen Layout",
+    "Race Condition",
+    "Keypad Conundrum",
+    "Monkey Market",
+    "LAN Party",
+    "Crossed Wires",
+    "Code Chronicle",
+];
+
+macro_rules! run_day {
+    ($x: ident) => {
+        let start1 = Instant::now();
+        let result1 = problems::$x::part_one();
+        let duration1 = start1.elapsed();
+
+        let start2 = Instant::now();
+        let result2 = problems::$x::part_two();
+        let duration2 = start2.elapsed();
+
+        println!("Part 1: solution: {} || time: {:?}", result1, duration1);
+        println!("Part 2: solution: {} || time: {:?}", result2, duration2);
     };
-    ($x: ident, 2) => {
-        problems::$x::part_two()
+}
+
+#[allow(unused_macros)]
+macro_rules! table_row {
+    ($x: ident) => {
+        let day = stringify!($x);
+        let problem_number: usize = day[3..].parse().unwrap();
+        let problem_name = PROBLEM_NAMES[problem_number - 1];
+
+        let start1 = Instant::now();
+        let result1 = problems::$x::part_one();
+        let duration1 = start1.elapsed();
+
+        let start2 = Instant::now();
+        let result2 = problems::$x::part_two();
+        let duration2 = start2.elapsed();
+
+        // println!("solutions: {result1} || {result2}");
+        println!("| {problem_number} | [{problem_name}](https://adventofcode.com/2024/day/{problem_number}) | [Source](src/problems/{day}.rs) | {duration1:?} \\| {duration2:?} |");
     };
 }
 
 fn main() {
-    let start = Instant::now();
-
-    let result = problems::day21::part_two();
-
-    let duration = start.elapsed();
-
-    println!("{result} in {duration:?}");
+    run_day!(day24);
 }
